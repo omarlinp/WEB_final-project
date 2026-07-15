7082ec514fbc13614dd57e22cb59c964e5134a94
omarlinp <omarlinp@gmail.com>
2026-07-15 12:35:56 -0400

    added the bootstrap module

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.set('views', path.join(__dirname, 'src/views'));
 //express configuration
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 
 app.get('/', async (req, res) =>{
     res.render('index')
