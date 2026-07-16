01bef7c654d4ff253350776c72d90b532014e24c
omarlinp <omarlinp@gmail.com>
2026-07-15 22:11:50 -0400

    created the functions to updated and delte users

--- src/models/users.js
+++ src/models/users.js
@@ -11,11 +11,35 @@ export async function getAllUsers() {
     }
 }
 export async function createUser(object) {
-    const {first_name,last_name,phone,email,profile_image,google_id,password} = object;
-    const values = [first_name,last_name,phone,email,profile_image,google_id,password];
-    const sql = `INSERT INTO users (first_name,last_name,phone,email,profile_image,google_id,password)
-                VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING id`;
+    const {first_name,last_name,phone,email,profile_image,google_id,password,username} = object;
+    const values = [first_name,last_name,phone,email,profile_image,google_id,password,username];
+    const sql = `INSERT INTO users (first_name,last_name,phone,email,profile_image,google_id,password,username)
+                VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING id`;
     let result = await query(sql,values);
     return result.rows[0];
 
+}
+export async function updateUser(object) {
+    const {first_name,last_name,phone,email,profile_image,google_id,password,id} = object;
+    const values = [first_name,last_name,phone,email,profile_image,google_id,password,id];
+    const sql = `UPDATE users SET 
+
+    first_name = $1,
+    last_name = $2,
+    phone = $3,
+    email = $4,
+    profile_image = $5,
+    google_id = $6,
+    password = $7,
+
+    WHERE id = $8`;
+    let result = await query(sql,values);
+    return result.rows[0];
+}
+export async function deleteUser(object) {
+    const {id} = object;
+    const values = [id]
+    const sql = `DELETE FROM users WHERE id = $1;`;
+    let result = await query(sql,values);
+    return result;
 }
