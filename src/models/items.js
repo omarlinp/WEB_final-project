71ee4cad3926339ffad3a10a01bc7f6d4a76056a
omarlinp <omarlinp@gmail.com>
2026-07-17 22:47:05 -0400

    modified the function getAllItem to display all the information and one image from the list. added new function to get items by user id

--- src/models/items.js
+++ src/models/items.js
@@ -3,7 +3,16 @@ import { query } from './db.js';
 
 export async function getAllItems() {
     try {
-        const sql = `SELECT * FROM items;`;
+        const sql = `SELECT DISTINCT ON (i.id) 
+                        i.id AS id,
+                        i.name AS name,
+                        i.price,
+                        i.description,
+                        i.user_id,
+                        i.province_id,
+                        img.image_path as image
+                        FROM public.items i
+                        LEFT JOIN public.item_imgs img ON i.id = img.item_id`;
         const result = await query(sql);
         return result.rows;
 
@@ -13,10 +22,26 @@ export async function getAllItems() {
     }
 }
 export async function getOneItem(id) {
-    const sql = `SELECT * FROM items WHERE id = $1 RETURNING id;`;
-    const result = await query(sql,id);
+    const sql = `SELECT * FROM items WHERE id = $1;`;
+    const result = await query(sql,[id]);
 
     return result;
+}
+export async function getItemsByUser(id) {
+    const sql = `SELECT DISTINCT ON (i.id) 
+    i.id AS id,
+    i.name AS name,
+    i.price,
+    i.description,
+    i.user_id,
+    i.province_id,
+    img.image_path AS image
+FROM public.items i
+LEFT JOIN public.item_imgs img ON i.id = img.item_id
+WHERE user_id = $1
+ORDER BY i.id, img.id ASC;`
+    const result = await query(sql,[id]);
+    return result.rows;
 }
 export async function createItem(object) {
     const {user_id,province_id,name,price,description, images = []} = object;
