38a7de9a7ab10a942da0f38dd383a4e111f50a7e
omarlinp <omarlinp@gmail.com>
2026-07-18 10:39:28 -0400

    updated the models to reflect the new database

--- src/models/items.js
+++ src/models/items.js
@@ -3,16 +3,7 @@ import { query } from './db.js';
 
 export async function getAllItems() {
     try {
-        const sql = `SELECT DISTINCT ON (i.id) 
-                        i.id AS id,
-                        i.name AS name,
-                        i.price,
-                        i.description,
-                        i.user_id,
-                        i.province_id,
-                        img.image_path as image
-                        FROM public.items i
-                        LEFT JOIN public.item_imgs img ON i.id = img.item_id`;
+        const sql = `SELECT * FROM items`;
         const result = await query(sql);
         return result.rows;
 
@@ -27,38 +18,20 @@ export async function getOneItem(id) {
     return result.rows[0];
 }
 export async function getItemsByUser(id) {
-    const sql = `SELECT DISTINCT ON (i.id) 
-    i.id AS id,
-    i.name AS name,
-    i.price,
-    i.description,
-    i.user_id,
-    i.province_id,
-    img.image_path AS image
-FROM public.items i
-LEFT JOIN public.item_imgs img ON i.id = img.item_id
-WHERE user_id = $1
-ORDER BY i.id, img.id ASC;`
+    const sql = `SELECT * FROM items
+                WHERE user_id = $1
+                ORDER BY created_date, img.id ASC;`
     const result = await query(sql,[id]);
     return result.rows;
 }
 export async function createItem(object) {
     const {user_id,province_id,name,price,description, images = []} = object;
     const values = [user_id,province_id,name,price,description];
-    const sql = `INSERT INTO items (user_id, province_id, name, price,description) VALUES ($1, $2, $3, $4, $5) RETURNING id`;
+    const sql = `INSERT INTO items (user_id, name, price,description,image_path) VALUES ($1, $2, $3, $4, $5) RETURNING id`;
     let result = await query(sql, values);
 
-    const newItemId = result.rows[0].id;
-    for (const image of images){
-        await createItemImage(image, newItemId);
-    }
-    return result;
+    return result.rows[0];
 
-}
-async function createItemImage(image,itemId){
-    const sql = `INSERT INTO item_imgs (image_path,item_id) VALUES ($1,$2);`
-    let result = await query(sql, [image,itemId]);
-    return result;
 }
 
 export async function updateItem(object) {
